741e933e9a7f7c6be6a7636b50b57528f043bbdf
mfatihaktas <mfatihaktas@gmail.com>
2014-05-30 19:38:56 -0400

    some additional control options added for gftps

--- gftp/run_gftp.sh
+++ gftp/run_gftp.sh
@@ -3,6 +3,7 @@ echo $1
 
 GFTP_DIR=/home/ubuntu/gftp
 PORT=5000
+CPORT=6000
 F=ltx.dat
 C_FILE=$GFTP_DIR/_$F
 S_FILE=10.39.1.12:$PORT$GFTP_DIR/$F
@@ -16,7 +17,8 @@ if [ $1  = 'c' ]; then
                   ftp://$S_FILE file://$C_FILE
                   #-tcp-buffer-size 1024000 \
 elif [ $1  = 's' ]; then
-  globus-gridftp-server -aa -password-file pwfile \
+  globus-gridftp-server -aa -password-file pwfile -c None \
+                        -control-interface 127.0.0.1:$CPORT
                         -log-module stdio -log-level info,warn,error -logfile $LOG_FILE \
                         -port $PORT
 elif [ $1  = 'glf' ]; then
